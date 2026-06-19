17fb73f3dbaed1bb88486aba47ce933e97829ff6
Paul Gomez <pauldro952@outlook.com>
2026-06-19 13:32:20 -0500

    Create File.php

---
+++ src/Files/File.php
@@ -0,0 +1,16 @@
+<?php namespace Pauldro\UtilityBelt\Files;
+// Base PHP
+use SplFileInfo;
+
+class File extends SplFileInfo {
+    public function countFiles() : int 
+    {
+        if ($this->isFile()) {
+            return 1;
+        }
+        if ($this->isDir() === false) {
+            return 0;
+        }
+        return count(glob($this->getPathname() . '/*', 0));
+    }
+}
